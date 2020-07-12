e4d31062352c714b33b6d435a5186549b1778614
Danil-k <ooo.kerm@yandex.ru>
2020-07-12 19:25:37 +0300

    1

--- src/main.js
+++ src/main.js
@@ -10,6 +10,50 @@ jQuery(function($) {
 
   //  MASKS OFF
 
+  // MENU BURGER SEARCH ON
+
+  $('.header__burger-wrap').on('click', function () {
+   $('.header').toggleClass('header-opened')
+  });
+
+  $('.header__personal-search').on('click', function () {
+    $('.header__personal-search').toggleClass('active')
+  });
+
+  // MENU BURGER SEARCH OFF
+
+
+  // SCROLL
+
+  if (window.matchMedia("(min-width: 1200px) and (max-width: 1599px)").matches) {
+
+    $(window).scroll(scrollArrow);
+
+    function scrollArrow() {
+      var scrolled = $(window).scrollTop();
+
+      if (scrolled > 160){
+        $('body').addClass('page-scrolled');
+      } else{
+        $('body').removeClass('page-scrolled');
+      }
+    }
+
+  } else if (window.matchMedia("(min-width: 1600px)").matches){
+
+    $(window).scroll(scrollArrow);
+
+    function scrollArrow() {
+      var scrolled = $(window).scrollTop();
+
+      if (scrolled > 160){
+        $('body').addClass('page-scrolled');
+      } else{
+        $('body').removeClass('page-scrolled');
+      }
+    }
+  }
+
 
 
 
